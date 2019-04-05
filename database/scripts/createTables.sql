819e7974f8baa82339694cac8eff08c31d117697
Joseph Wilson <32886201+jaw0608@users.noreply.github.com>
2019-04-05 00:12:32 -0400

    Updated Table Script

--- database/scripts/createTables.sql
+++ database/scripts/createTables.sql
@@ -2,53 +2,96 @@ USE database;
 
 CREATE TABLE IF NOT EXISTS Seller (
   ID int AUTO_INCREMENT,
-  Name varchar(30),
-  Phone BIGINT
+  Name varchar(30) NOT NULL,
+  Phone BIGINT NOT NULL,
   PRIMARY KEY (ID)
 );
 CREATE TABLE IF NOT EXISTS Customer (
   ID int AUTO_INCREMENT,
-  F_Name varchar(50),
-  L_Name varchar(50),
-  Email varchar(320),
-  Phone BIGINT,
+  F_Name varchar(50) NOT NULL,
+  L_Name varchar(50) NOT NULL,
+  Email varchar(320) NOT NULL,
+  Phone BIGINT NOT NULL,
   PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS Address(
-  Customer_ID int,
-  Address_ID int AUTO_INCREMENT,
-  City varchar(30),
-  State char(2),
-  Street_Number varchar(6),
-  Apt_Number var(7),
+  Customer_ID int NOT NULL,
+  Address_ID int AUTO_INCREMENT NOT NULL,
+  City varchar(30) NOT NULL,
+  State char(2) NOT NULL,
+  Street_Number varchar(6) NOT NULL,
+  Apt_Number char(5),
   FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
   PRIMARY KEY (Address_ID)
 );
-
+CREATE TABLE IF NOT EXISTS Item (
+  Number int,
+  Name varchar(20) NOT NULL,
+  Description varchar(500),
+  PRIMARY KEY (Number)
+);
 CREATE TABLE IF NOT EXISTS Payment(
   Customer_ID int,
-  Payment_Type int,
-  Card_Number BIGINT,
-  Card_Expiration DATE,
+  Payment_Type char(2) NOT NULL CHECK (Payment_Type in ('GC','CC','DC')),
+  Card_Number BIGINT NOT NULL,
+  Card_Expiration DATE NOT NULL,
   PRIMARY KEY (Customer_ID, Card_Number)
 );
-CREATE TABLE IF NOT EXISTS Payment_Types(
-  ID int,
-  Value char(2)
+CREATE TABLE IF NOT EXISTS Orders(
+  Number BIGINT NOT NULL,
+  Shipment_ID int NOT NULL,
+  Customer_ID int NOT NULL,
+  Seller_ID int NOT NULL,
+  Item_ID int NOT NULL,
+  Quantity int NOT NULL,
+  FOREIGN KEY (Shipment_ID) REFERENCES Shipment(ID),
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
+  FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
+  FOREIGN KEY (Item_ID) REFERENCES Item(ID),
+  PRIMARY KEY (Number,Shipment_ID,Customer_ID,Seller_ID,Item_ID)
 );
-INSERT INTO Payment_Types VALUES(
-  (1, 'CC'),
-  (2, 'DC'),
-  (3, 'GC')
+CREATE TABLE IF NOT EXISTS Inventory(
+  Item_ID int NOT NULL,
+  Seller_ID int NOT NULL,
+  Quantity int NOT NULL,
+  Price double NOT NULL,
+  FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
+  FOREIGN KEY (Item_ID) REFERENCES Item(ID),
+  PRIMARY KEY (Item_ID, Seller_ID)
 );
-CREATE TABLE IF NOT EXISTS Order(
-  ID int,
-  Type ch
-);
-
 CREATE TABLE IF NOT EXISTS Shipment(
-  
-
+  ID int AUTO_INCREMENT NOT NULL,
+  Address_ID int NOT NULL,
+  Customer_ID int NOT NULL,
+  Carrier varchar(10) NOT NULL,
+  Speed varchar(9) NOT NULL CHECK (Speed in ('Expedited', 'Standard')),
+  FOREIGN KEY (Address_ID) REFERENCES Address(ID),
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
+  PRIMARY KEY (ID)
 );
 
+CREATE TABLE IF NOT EXISTS Reviews(
+  Customer_ID int NOT NULL,
+  Seller_ID int NOT NULL,
+  Item_ID int NOT NULL,
+  Rating int NOT NULL,
+  CHECK (Rating>=0 AND Rating<=5),
+  Review varchar(500) NOT NULL,
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
+  FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
+  FOREIGN KEY (Item_ID) REFERENCES Item(ID),
+  PRIMARY KEY (Customer_ID, Seller_ID, Item_ID)
+);
+CREATE TABLE IF NOT EXISTS Cart(
+  Customer_ID int NOT NULL,
+  Item_ID int NOT NULL,
+  Quantity int NOT NULL,
+  Seller_ID int NOT NULL,
+  Price double NOT NULL,
+  FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
+  FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
+  FOREIGN KEY (Item_ID) REFERENCES Item(ID),
+  PRIMARY KEY (Customer_ID, Seller_ID, Item_ID)
+);
+                                  
