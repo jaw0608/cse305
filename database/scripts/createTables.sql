1be76de576065fb9c6df247d26b1b9985672e0d6
Joseph Wilson <32886201+jaw0608@users.noreply.github.com>
2019-04-09 14:59:24 -0400

    Finished create tables for Hw2

--- database/scripts/createTables.sql
+++ database/scripts/createTables.sql
@@ -17,39 +17,42 @@ CREATE TABLE IF NOT EXISTS Customer (
 
 CREATE TABLE IF NOT EXISTS Address(
   Customer_ID int NOT NULL,
-  Address_ID int AUTO_INCREMENT NOT NULL,
+  ID int AUTO_INCREMENT NOT NULL,
   City varchar(30) NOT NULL,
   State char(2) NOT NULL,
-  Street_Number varchar(6) NOT NULL,
-  Apt_Number char(5),
+  Street_Number int NOT NULL,
+  Apt_Number int NOT NULL,
   FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
-  PRIMARY KEY (Address_ID)
+  PRIMARY KEY (ID)
 );
 CREATE TABLE IF NOT EXISTS Item (
-  Number int,
+  ID int,
   Name varchar(20) NOT NULL,
   Description varchar(500),
-  PRIMARY KEY (Number)
+  PRIMARY KEY (ID)
 );
 CREATE TABLE IF NOT EXISTS Payment(
   Customer_ID int,
+  ID int AUTO_INCREMENT NOT NULL,
   Payment_Type char(2) NOT NULL CHECK (Payment_Type in ('GC','CC','DC')),
   Card_Number BIGINT NOT NULL,
   Card_Expiration DATE NOT NULL,
-  PRIMARY KEY (Customer_ID, Card_Number)
+  PRIMARY KEY (ID)
 );
 CREATE TABLE IF NOT EXISTS Orders(
-  Number BIGINT NOT NULL,
+  ID BIGINT AUTO_INCREMENT NOT NULL,
   Shipment_ID int NOT NULL,
   Customer_ID int NOT NULL,
   Seller_ID int NOT NULL,
   Item_ID int NOT NULL,
+  Payment_ID int NOT NULL,
   Quantity int NOT NULL,
   FOREIGN KEY (Shipment_ID) REFERENCES Shipment(ID),
   FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
   FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
   FOREIGN KEY (Item_ID) REFERENCES Item(ID),
-  PRIMARY KEY (Number,Shipment_ID,Customer_ID,Seller_ID,Item_ID)
+  FOREIGN KEY (Payment_ID) REFERENCES Payment(Payment_ID),
+  PRIMARY KEY (ID,Customer_ID,Seller_ID,Item_ID)
 );
 CREATE TABLE IF NOT EXISTS Inventory(
   Item_ID int NOT NULL,
@@ -88,10 +91,17 @@ CREATE TABLE IF NOT EXISTS Cart(
   Item_ID int NOT NULL,
   Quantity int NOT NULL,
   Seller_ID int NOT NULL,
-  Price double NOT NULL,
   FOREIGN KEY (Customer_ID) REFERENCES Customer(ID),
   FOREIGN KEY (Seller_ID) REFERENCES Seller(ID),
   FOREIGN KEY (Item_ID) REFERENCES Item(ID),
   PRIMARY KEY (Customer_ID, Seller_ID, Item_ID)
 );
-                                  
+CREATE TABLE IF NOT EXISTS Employee(
+  ID int AUTO_INCREMENT NOT NULL,
+  Supervisor_ID int,
+  F_Name varchar(20) NOT NULL,
+  L_NAME varchar(20) NOT NULL,
+  Role varchar(40) NOT NULL,
+  FOREIGN KEY (Supervisor_ID) REFERENCES Employee(ID),
+  PRIMARY KEY (ID)
+);
